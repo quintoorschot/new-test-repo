26fc1f54c608666a69d48b9d814cf4ccbdfcd40e
quintoorschot <quintvanoorschot@gmail.com>
2025-11-19 14:49:54 +0100

    initial

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void listsFiles_flaky() throws IOException {
+    Path dir = Files.createTempDirectory("d-");
+    Files.createFile(dir.resolve("a.txt"));
+    Files.createFile(dir.resolve("b.txt"));
+    Files.createFile(dir.resolve("c.txt"));
+
+    // This is the flaky part: File.list() does not guarantee order.
+    List<String> names = Arrays.asList(dir.toFile().list());
+
+    assertEquals(Arrays.asList("a.txt", "b.txt", "c.txt"), names);
+  }
+
+  @Test
+  public void listsFiles_flaky2() throws IOException {
+    Path dir = Files.createTempDirectory("d-");
+    Files.createFile(dir.resolve("d.txt"));
+    Files.createFile(dir.resolve("e.txt"));
+    Files.createFile(dir.resolve("f.txt"));
+
+    // This is the flaky part: File.list() does not guarantee order.
+    List<String> names = Arrays.asList(dir.toFile().list());
+
+    assertEquals(Arrays.asList("d.txt", "e.txt", "f.txt"), names);
+  }
+}
