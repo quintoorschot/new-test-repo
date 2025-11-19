79ccf3f0c04b2e92e64f62ec412c0a63cce4eebd
quintoorschot <quintvanoorschot@gmail.com>
2025-11-19 17:33:00 +0100

    m

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.junit.Test;
 
 public class AppTest {
-//
+
   @Test
   public void listsFiles_flaky() throws IOException {
     Path dir = Files.createTempDirectory("d-");
